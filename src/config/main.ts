6bce777d246e958aa5bae956d4d0a028a8a3cc79
Heisenberg <alexrayo.net@gmail.com>
2023-04-21 18:47:06 -0600

    adding text to whatsapp link

--- src/config/main.ts
+++ src/config/main.ts
@@ -1,5 +1,5 @@
 const CONTACT_NUMBER = '+526658452790'
 
 export const contact = {
-  WHATSAPP_LINK: `https://wa.me/${CONTACT_NUMBER}`
+  WHATSAPP_LINK: `https://wa.me/${CONTACT_NUMBER}?text=Hola,%20me%20gustaría%20cotizar%20un%20diseño`
 }

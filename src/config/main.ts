6468a34a90130f884d7a77ae345ca94b021c89bf
Heisenberg <yutarunet@gmail.com>
2025-02-27 10:32:41 -0600

    [update]contact

--- src/config/main.ts
+++ src/config/main.ts
@@ -1,4 +1,4 @@
-const CONTACT_NUMBER = '+526658452790'
+const CONTACT_NUMBER = '+50584991528'
 
 export const contact = {
   WHATSAPP_LINK: `https://wa.me/${CONTACT_NUMBER}?text=Hola,%20me%20gustaría%20cotizar%20un%20diseño`
